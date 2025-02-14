f2a0521fad701ab7386750db4bbc933a91755efa
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-02-14 16:50:14 -0300

    Estilizações com Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import Home from "./pages/home/Home"
+import NavBar from "./components/navbar/NavBar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
 
 function App() {
 
 
   return (
     <>
-    <Home />
+      <BrowserRouter>
+
+        <NavBar />
+
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
+        
+      </BrowserRouter>
     </>
-  
   )
 }
 
