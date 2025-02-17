d56959654d73925a0a32f69b860f24417345e566
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-02-17 10:08:56 -0300

    estilização das telas de login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import Home from "./pages/home/Home"
 import NavBar from "./components/navbar/NavBar"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
+import Home from "./pages/home/Home"
 
 function App() {
 
@@ -14,8 +16,10 @@ function App() {
 
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
           </Routes>
         </div>
 
