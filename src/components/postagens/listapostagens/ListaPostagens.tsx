99a1f0f1c7779779895dfba11ee58a925093ed18
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-02-20 18:10:11 -0300

    feat: finalização do CRUD de postagem

--- src/components/postagens/listapostagens/ListaPostagens.tsx
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -1,14 +1,79 @@
+import { useNavigate } from "react-router-dom";
 import CardPostagens from "../cardpostagens/CardPostagens";
+import { useState, useContext, useEffect } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import Postagem from "../../../models/Postagem";
+import { buscar } from "../../../services/Service";
+import { DNA } from "react-loader-spinner";
 
 function ListaPostagens() {
+
+    const navigate = useNavigate();
+
+    const [postagens, setPostagens] = useState<Postagem[]>([]);
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const { usuario, handleLogout } = useContext(AuthContext);
+    const token = usuario.token;
+
+    async function buscarPostagens() {
+        setIsLoading(true)
+
+        try {
+            await buscar('/postagens', setPostagens, {
+                headers: {
+                    Authorization: token,
+                },
+            })
+
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+
+        }finally{
+            setIsLoading(false)
+        }
+
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado')
+            navigate('/');
+        }
+    }, [token])
+
+    useEffect(() => {
+        buscarPostagens()
+    }, [postagens.length])
+
     return (
         <>
+            {isLoading && (
+                <DNA
+                    visible={true}
+                    height="200"
+                    width="200"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper mx-auto"
+                />
+            )}
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col mx-2">
+                    {(
+                        (!isLoading && postagens.length === 0) && (
+                            <span className="text-3xl text-center my-8"> Nenhuma postagem foi encontrada.</span>
+                        )
+                    )}
                     <div className='container mx-auto my-4 
-                        grid grid-cols-1 md:grid-cols-2 
-                        lg:grid-cols-3 gap-4'>
-                        <CardPostagens />
+                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+                    >
+                        {postagens.map((postagem) => (
+                            <CardPostagens key={postagem.id} postagem={postagem} />
+                        ))}
                     </div>
                 </div>
             </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,15 @@ import ListaPostagens from "../../components/postagens/listapostagens/ListaPosta
 import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
 
 function Home() {
+
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
-                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                    <div className="flex flex-col gap-4 items-center
+                                    justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vindo!
+                            Seja Bem Vinde!
                         </h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
@@ -21,16 +23,15 @@ function Home() {
                         </div>
                     </div>
 
-                    <div className="flex justify-center ">
+                    <div id="imagem" className="flex justify-center">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
-                            alt="Imagem Página Home"
-                            className='w-2/3'
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            className="w-2/3"
                         />
                     </div>
                 </div>
             </div>
-
             <ListaPostagens />
         </>
     )
